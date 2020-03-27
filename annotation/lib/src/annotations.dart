dc464bab85cf4e990731ba379cf05d325da1b244
Artem Ufimtcev <alotxo@gmail.com>
2020-03-27 22:40:25 +0100

    Add ignore generated files and ignore private files options to the Header annotation class

--- annotation/lib/src/annotations.dart
+++ annotation/lib/src/annotations.dart
@@ -1,6 +1,12 @@
 class Header {
 
   final String glob;
+  final bool ignoreGeneratedFiles;
+  final bool ignorePrivateFiles;
 
-  const Header({this.glob});
+  const Header({
+    this.glob,
+    this.ignoreGeneratedFiles = true,
+    this.ignorePrivateFiles = false
+  });
 }
